ef13f9d8231682a09cba628d0515f4202626d195
Victoria Fluharty <victoria.fluharty@gmail.com>
2020-12-04 14:37:36 -0600

    adds 2020/day-03 solution

---
+++ 2020/day-03/solution.rb
@@ -0,0 +1,34 @@
+def get_tree_map
+  File.read("2020/day-02/input.txt").split("\n").map(&:chars).map { |line| line * 100 }
+end
+
+def trajectory_issues(tree_map, slopes)
+  result = slopes.map do |slope|
+    trees_hit = 0
+
+    x = 0
+    y = 0
+
+    while y < tree_map.size
+      trees_hit += 1 if tree_map[y][x] == '#'
+      x += slope[0]
+      y += slope[1]
+    end
+
+    trees_hit
+  end.reduce { |n, res| n * res }
+end 
+
+# Part 1
+slopes1 = [[3, 1]]
+trajectory_issues(get_tree_map, slopes1)
+
+# Part 2
+slopes2 = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2]
+]
+trajectory_issues(get_tree_map, slopes2)
